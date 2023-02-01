699aa9e5e347b62bafbf23224e60ab383e976cba
Gorana <greenkoala1907@gmail.com>
2023-02-01 20:51:51 +0100

    Kreiran navBar i instalirane biblioteke za ikonice

---
+++ src/components/NavBar.jsx
@@ -0,0 +1,26 @@
+import { Link } from 'react-router-dom';
+import { BsSearch } from 'react-icons/bs';
+function NavBar() {
+  return (
+    <div>
+        <nav className="nav">
+            <div className="nav__title"><Link to='/'><h1>HEALTHY FOOD RECIPES</h1></Link></div>
+                <ul className="nav__list">
+
+                    <li className="nav__item">   <Link to='/'>Pocetna </Link></li>
+                    <li className="nav__item"> <Link to='/recepti'>Recepti</Link></li>
+                    <li className="nav__item"> 
+
+                        <input type="text" id="kriterijum" placeholder=" " name="search"/>
+                        <button type="submit" className='dugmePretraga'  ><BsSearch></BsSearch></button>
+
+
+
+                    </li>
+                </ul>
+        </nav>
+    </div>
+  );
+}
+
+export default NavBar;
